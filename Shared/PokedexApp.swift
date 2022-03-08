a93c2fdb54b126506ec78bdda36c053db8429e61
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2022-03-07 20:05:30 -0600

    Initial Commit

---
+++ Shared/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Shared
+//
+//  Created by Rodolfo Galarza Jr on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

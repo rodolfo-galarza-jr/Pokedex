71a5e06afe21ddd611ed30cfaa8bc58098d02b44
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2022-03-11 21:08:32 -0600

    add NetworkingManager Protocol

--- Shared/Network/NetworkingManager.swift
+++ Shared/Network/NetworkingManager.swift
@@ -6,48 +6,53 @@
 //
 
 import Foundation
+import SwiftUI
 
-enum NetworkingManager {
+
+enum networkingError: LocalizedError {
+    case badURLResponse(url: URL, statusCode: Int)
+    case urlResponseFailed(url: URL)
+    case jsonDecodingFailed
+    case unknown
     
-    enum networkingError: LocalizedError{
-        case badURLResponse(url: URL, statusCode: Int)
-        case urlResponseFailed(url: URL)
-        case jsonDecodingFailed
-        case unknown
-        
-        var errorDescription: String?{
-            switch self {
-            case .badURLResponse(url: let url, statusCode: let statusCode):
-                return "bad response from URL \(url) with status code: \(statusCode)"
-            case .urlResponseFailed(url: let url):
-                return "response failed from URL: \(url)"
-            case .jsonDecodingFailed:
-                return "JSON decoding failure"
-            case .unknown:
-                return "unknown error occurred"
-            }
+    var errorDescription: String? {
+        switch self {
+        case .badURLResponse(url: let url, statusCode: let statusCode):
+            return "bad response from URL: \(url) with status code: \(statusCode)"
+        case .urlResponseFailed(url: let url):
+            return "response failed from URL: \(url)"
+        case .jsonDecodingFailed:
+            return "JSON decoding failure"
+        case .unknown:
+            return "unknown error occurred"
         }
     }
+}
+
+protocol NetworkingManagerProtocol {
+    func fetch<T: Decodable>(type: T.Type, url: String) async throws -> T
+}
+
+struct NetworkingManager: NetworkingManagerProtocol {
     
-    static func fetch<T: Decodable>(type: T.Type, url: String) async throws -> T {
-        
-        let url = URL(string: url)!
+    func fetch<T>(type: T.Type, url: String) async throws -> T where T : Decodable {
+        guard let url = URL(string: url) else {
+            preconditionFailure("Invalid URL format")
+        }
         
         let (data, response) = try await URLSession.shared.data(from: url)
-                
-        guard let httpResponse = response as? HTTPURLResponse else {
+        
+        guard let httpResonse = response as? HTTPURLResponse else {
             throw networkingError.urlResponseFailed(url: url)
         }
         
-        guard httpResponse.statusCode == 200 else {
-            throw networkingError.badURLResponse(url: url, statusCode: httpResponse.statusCode)
+        guard httpResonse.statusCode == 200 else {
+            throw networkingError.badURLResponse(url: url, statusCode: httpResonse.statusCode)
         }
-    
-        do
-        {
+        
+        do {
             return try JSONDecoder().decode(type, from: data)
         } catch {
-            print(error)
             throw networkingError.jsonDecodingFailed
         }
     }

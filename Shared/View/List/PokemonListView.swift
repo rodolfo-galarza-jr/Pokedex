1525ec849530ab52054c2e2e0ae346282a580d2d
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2022-04-02 22:30:12 -0500

    add segue to detail view

--- Shared/View/List/PokemonListView.swift
+++ Shared/View/List/PokemonListView.swift
@@ -11,15 +11,15 @@ struct PokemonListView: View {
     
     @ObservedObject var viewModel = PokemonListViewModel()
     
+    @State private var showDetailView: Bool = false
+    @State private var selectedPokemon: Pokemon? = nil
     
     var body: some View {
         NavigationView {
-            
             VStack {
                 switch viewModel.state {
                 case .success:
                     VStack {
-                        
                         Text("Pok\u{00E9}mon")
                             .font(.title)
                             .fontWeight(.bold)
@@ -36,24 +36,42 @@ struct PokemonListView: View {
                     EmptyView()
                 }
             }
-
             .navigationBarTitleDisplayMode(.inline)
             .navigationBarHidden(true)
         }.task {
             await viewModel.getPokemonList()
         }
     }
+    
+    private func segue(pokemon: Pokemon) {
+        selectedPokemon = pokemon
+        showDetailView.toggle()
+    }
 }
 
 extension PokemonListView {
     private var PokemonList: some View {
-        List {
-            ForEach(self.viewModel.filteredPokemonList) { pokemon in
-                PokemonRowView(pokemonName: pokemon.name, pokemonImgURL: pokemon.sprites.other.official.frontDefault, pokemonID: pokemon.id, types: pokemon.types)
+        ScrollView(showsIndicators: false) {
+            LazyVStack {
+                ForEach(self.viewModel.filteredPokemonList) { pokemon in
+                    PokemonRowView(pokemonName: pokemon.name, pokemonImgURL: pokemon.sprites.other.official.frontDefault, pokemonID: pokemon.id, types: pokemon.types)
+                        .onTapGesture {
+                            segue(pokemon: pokemon)
+                        }
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 2)
+                }
+                .listRowSeparator(.hidden)
             }
-            .listRowSeparator(.hidden)
+            .background(
+                NavigationLink(
+                    destination: PokemonDetailLoadingView(pokemon: $selectedPokemon),
+                    isActive: $showDetailView,
+                    label: {
+                        EmptyView()
+                    })
+            )
         }
-        .listStyle(.plain)
     }
 }
 

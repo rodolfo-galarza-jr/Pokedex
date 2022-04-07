3855d95d8d15458faa6f06cf0d0f24157f23adbf
Rodolfo Galarza Jr <rodolfoxgalarza@gmail.com>
2022-04-07 18:31:12 -0500

    data view for detail view

---
+++ Shared/View/Detail/About/PokemonDetailDataView.swift
@@ -0,0 +1,34 @@
+//
+//  PokemonDetailDataView.swift
+//  Pokedex (iOS)
+//
+//  Created by Rodolfo Galarza Jr on 4/6/22.
+//
+
+import SwiftUI
+
+struct PokemonDetailDataView: View {
+    let dataName: String
+    let dataValue: String
+    let typeColor: String
+    var body: some View {
+        HStack(spacing: 30) {
+            Text(dataName)
+                .font(.headline)
+                .foregroundColor(Color(typeColor))
+            
+            Text(dataValue)
+                .font(.headline)
+            
+            Spacer()
+        }
+        .padding(8)
+    }
+}
+
+struct PokemonDetailDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailDataView(dataName: "Height", dataValue: "\(dev.pokemon.height)", typeColor: "grass")
+            .previewLayout(.sizeThatFits)
+    }
+}
